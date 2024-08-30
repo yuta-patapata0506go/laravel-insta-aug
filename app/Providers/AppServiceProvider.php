1830bf6eacdfa23418618b69083c8a74967ea2b5
奥畑雄太 <okuhatayuuta@okuhatayuutanoMacBook-Pro.local>
2024-08-30 21:35:41 +0900

    added safe code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,9 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
